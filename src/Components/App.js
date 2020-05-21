5ad4983e24be25a8d59b9da9409bcab6d4047765
Khusharth <khusharth19@gmail.com>
2020-05-22 02:05:14 +0530

    Adding Routing of streams pages

---
+++ src/Components/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import StreamCreate from './streams/StreamCreate';
+import StreamList from './streams/StreamList';
+import StreamEdit from './streams/StreamEdit';
+import StreamShow from './streams/StreamShow';
+import StreamDelete from './streams/StreamDelete';
+import Header from "./Header";
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div className="ui container">
+                <Header />
+                <Route path="/" exact component={StreamList} />
+                <Route path="/streams/new" exact component={StreamCreate} />
+                <Route path="/streams/edit" exact component={StreamEdit} />
+                <Route path="/streams/delete" exact component={StreamDelete} />
+                <Route path="/streams/show" exact component={StreamShow} />
+            </div>
+        </BrowserRouter>
+    );
+};
+
+export default App;
